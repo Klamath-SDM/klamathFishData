60116cad92cc9648168e1947cff549eb10c9adf0
Badhia <byuneskatz@flowwest.com>
2025-06-26 14:59:38 -0700

    delets unecessary code

--- data-raw/processing-scripts/process_salmonid_data.R
+++ data-raw/processing-scripts/process_salmonid_data.R
@@ -7,47 +7,6 @@ library(pins)
 library(readxl)
 library(rivermile)
 
-# library(googlesheets4)
-# note that if we want to source directly from google sheets we need to make the sheet public, lets discuss if we
-# want want to do that so that updates are done directly on the sheet (?)
-# for now I am downloading spredsheets to tables_with_data folder
-# rst <- read_sheet("https://docs.google.com/spreadsheets/d/1rk1uoicdGNwcT6UKDLlQr7YkX9W5Fh2FCea3FAxxbGg",
-           # sheet = "RST")
-# using sub-basin shp to assign sub_basin names to other data
-# sub_basin <- st_read("data-raw/tables_with_data/sub-basin-boundaries/Klamath_HUC8_Subbasin.shp")
-
-# function to assign sub-basin to datasets #TODO let's consider moving this function to an R package(?)
-# assign_sub_basin <- function(data, sub_basin, is_point = TRUE, lon_col = "longitude", lat_col = "latitude", sub_basin_col = "NAME") {
-#   if (is_point) {
-#     sf_data <- st_as_sf(data, coords = c(lon_col, lat_col), crs = 4326)
-#   } else {
-#     sf_data <- st_as_sf(data)
-#   }
-#   sf_data <- sf_data |>
-#     st_transform(st_crs(sub_basin)) |>
-#     st_join(sub_basin[sub_basin_col]) |>
-#     rename(sub_basin = !!sub_basin_col) |>
-#     mutate(sub_basin = tolower(sub_basin))
-#   if (is_point) {
-#     coords <- st_coordinates(sf_data)
-#     sf_data[[lon_col]] <- coords[, 1]
-#     sf_data[[lat_col]] <- coords[, 2]
-#     sf_data <- st_drop_geometry(sf_data)
-#   }
-#   return(sf_data)
-# }
-
-
-# habitat data ---- #TODO check with Maddee on source of this table -this has to be moved to klamathHabitatData
-# habitat_data <- read_csv(here::here('data-raw','tables_with_data','habitat_data.csv')) |>
-#   clean_names() |>
-#   mutate(longitude = as.numeric(longtidue)) |>
-#   rename(stream = river) |>
-#   assign_sub_basin(sub_basin) |>
-#   select(-longtidue) |>
-#   select(stream, sub_basin, everything()) |>
-#   glimpse()
-
 # fisheries_location_lookup  ----
 # RST
 rst_sites <- read_csv(here::here('data-raw','tables_with_data', 'rst_sites.csv')) |>
