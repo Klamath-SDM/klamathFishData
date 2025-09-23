fc2f93d5f48e0c892729061dcbd93c3584bbfca2
alvizek <avizek@flowwest.com>
2025-09-23 14:56:53 -0700

    updates to fisheries model estimates processing

--- R/data.R
+++ R/data.R
@@ -5,14 +5,15 @@
 #' is not species or location specific.
 #' Data exploration was done in R Markdowns which also contain information on data source.
 #' {Modeled Fisheries Data R Markdown}{https://github.com/Klamath-SDM/KlamathEDA/blob/main/data-raw/fisheries/modeled/modeled-fisheries-data.md}
-#' @format A tibble with 127 rows and 12 columns
+#' @format A tibble with 1034 rows and 14 columns
 #' \itemize{
-#'   \item \code{julian_year}: julian year
-#'   \item \code{stream}: stream (INSERT LINK TO DOCUMENTATION for location here)
-#'   \item \code{species}: species (coho, lost river lakeshore spawning, lost river river spawning, shortnose)
-#'   \item \code{lifestage}: lifestage category (parr, smolts, adult)
+#'   \item \code{julian_year}: julian year. Min/max years vary by location, species, type of estimate. Adult escapement is reported for a brood year which is the same as the julian year. Juvenile abundance typically spans multiple years (e.g. Oct 2020 - May 2021) and would be assigned a julian year for the latter part of this period.
+#'   \item \code{stream}: stream ("bogus creek", "iron gate hatchery  (igh)", "klamath river", "lower klamath river", "other tributaries", "salmon river", "scott river", "shasta river", "trinity river", "trinity river hatchery (trh)", "upper klamath lake", "yurok and hoopa reservation tribs")
+#'   \item \code{species}: species (coho salmon, winter steelhead, steelhead, fall chinook salmon, spring chinook salmon, lost river lakeshore spawning, lost river river spawning, shortnose)
+#'   \item \code{origin}: origin (natural, hatchery, mixed, unknown)
+#'   \item \code{lifestage}: lifestage category (parr, smolts, adult, yoy, age 1+, age 2+, adult and subadult, spawner)
 #'   \item \code{sex}: sex, if applicable to model (female, male, NA)
-#'   \item \code{estimate_type}: type of model estimate (abundance, apparent survival, seniority probability, annual population rate of change)
+#'   \item \code{estimate_type}: type of model estimate (abundance, redd abundance, count, apparent survival, seniority probability, annual population rate of change)
 #'   \item \code{estimate}: value of estimate
 #'   \item \code{confidence_interval}: type of confidence or credible interval (e.g., 80, 95)
 #'   \item \code{lower_bounds_estimate}: value of lower bounds estimate, if applicable
@@ -20,6 +21,7 @@
 #'   \item \code{estimation_method}: type of model used
 #'   \item \code{is_complete_estimate}: data included in model are complete. In some cases, it is known that an
 #'   estimate is not complete, otherwise it is assumed that estimate is complete.
+#'   \item \code{source}: describes where data were sourced from.
 #'   }
 'fisheries_model_estimates'
 
