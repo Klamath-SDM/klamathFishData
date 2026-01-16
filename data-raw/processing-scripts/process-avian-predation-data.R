aa839fe7d0d8c97158ab6be3dd2c8cc476b2e0df
Badhia <byuneskatz@flowwest.com>
2026-01-15 16:36:41 -0800

    adds notes and todos for remaning avian predation 2024 data

--- data-raw/processing-scripts/process-avian-predation-data.R
+++ data-raw/processing-scripts/process-avian-predation-data.R
@@ -17,12 +17,23 @@ tables <- extract_tables(pdf_path, method = "stream", output = "tibble")
 pdf_path_2024 <- "data-raw/Avian-Predation-Final-Summary-Report_2024.pdf"
 tables_2024 <- extract_tables(pdf_path_2024, method = "stream", output = "tibble")
 
-#### PIT-tagged LRS, SNS, KLS, SNS-KLS and SARP ----
+
+#### ======= PIT-tagged LRS, SNS, KLS, SNS-KLS and SARP ===== ####
+# The first number = the number of PIT-tagged fish available to avian predators that year in that location.
+# This is essentially how many tagged suckers (or salmon) were released or present in the waterbody.
+# The number in parentheses = the number of those tags recovered from avian predator nesting colonies in the same year.
+# Recovery = physical detection of PIT tags on bird colonies (e.g., cormorants, pelicans) after birds consumed tagged fish.
+# Importantly, these are minimum counts of predation events — not corrected for imperfect detection or deposition, so the true number of fish eaten could be higher.
+
+#  code below reads table 2 of 2021-2023 report, and table 2 of 2024 report
+
+# (1) read in 2021-2023 report table 2
 tag_data_raw <- tables[[2]] |> glimpse()
 
 tag_data_raw <- as.data.frame(tag_data_raw, stringsAsFactors = FALSE) %>%
   clean_names()
 
+# (2) read in 2024 report table 2
 tag_data_raw_2 <- tables_2024[[2]] |> glimpse()
 
 tag_data_raw_2_clean <- tag_data_raw_2 |>
@@ -34,13 +45,11 @@ tag_data_raw_2_clean <- tag_data_raw_2 |>
     extra = "merge") |>
   glimpse()
 
-#TODO continue clening here
+# TODO (3)
+# 2024 looks slightly different than 2021-2023 so combining and re-running code below won't work
+# 2024 table needs to be cleaned and combined with 2021-2023
 
-# The first number = the number of PIT-tagged fish available to avian predators that year in that location.
-# This is essentially how many tagged suckers (or salmon) were released or present in the waterbody.
-# The number in parentheses = the number of those tags recovered from avian predator nesting colonies in the same year.
-# Recovery = physical detection of PIT tags on bird colonies (e.g., cormorants, pelicans) after birds consumed tagged fish.
-# Importantly, these are minimum counts of predation events — not corrected for imperfect detection or deposition, so the true number of fish eaten could be higher.
+# (4) functions to clean 2021-2023 table
 parse_avail <- function(x) {
   # available number before parentheses, allowing commas
   v <- str_extract(x, "^[0-9,]+")
@@ -98,6 +107,8 @@ avian_predation_pit_tag <- tag_data_clean |>
   # mutate(recovered_rate = if_else(!is.na(available) & available > 0, recovered / available, NA_real_))
   glimpse()
 
+# TODO combine with 2024 once cleaned
+# (4) cleaned and NOT combined predation_estimates_avian_pit_tag
 predation_estimates_avian_pit_tag <- avian_predation_pit_tag |>
   mutate(species = case_when(str_detect(fish_group, "LRS") ~ "lost river sucker",
                              str_detect(fish_group, "SNS-KLS") ~ "shortnose and klamath largescale suckers",
@@ -126,11 +137,15 @@ predation_estimates_avian_pit_tag <- avian_predation_pit_tag |>
   select(-available, -recovered) |>
   glimpse()
 
+#### ======= estimate predation rates - LRS, SNS, KLS, SNS-KLS and SARP (UKL and Clear Lake) ===== ####
 
-#### estimate predation rates - LRS, SNS, KLS, SNS-KLS and SARP (UKL and Clear Lake) ----
 # Estimates of predation rates (95% credible intervals) on PIT-tagged Lost River suckers (LRS),
 # Shortnose suckers (SNS), Klamath Largescale suckers (KLS), Shortnose/Klamath Largescale suckers (SNS-KLS), and wild juvenile suckers by piscivorous colonial waterbirds nesting at colonies in Upper Klamath
 # Lake and Clear Lake Reservoir (i.e., cumulative predation effects)
+
+#  code below reads table 3 of 2021-2023 report, and table 3 of 2024 report
+
+# (1) read in 2021-2023 report table 3
 estimate_predation_raw <- tables[[3]] |> glimpse()
 
 pred_raw_clean <- estimate_predation_raw |>
@@ -159,8 +174,7 @@ pred_raw_clean[8, c("CLR_Adult_SNS", "CLR_Adult_KLS", "CLR_Wild_Juv")] <- # migr
 names(pred_raw_clean)[2:5] <- c("UKL_Adult_LRS", "UKL_Adult_SNS", "UKL_Adult_KLS", "UKL_Juveniles")
 names(pred_raw_clean)[6:8] <- c("CLR_Adult_LRS", "CLR_Adult_SNS_KLS", "CLR_Juveniles")
 
-# ---- BINDING 2024 ----
-# =============== BINDING 2024 ======================= #
+# (2) read in 2024 report table 3
 # using tabulizer::extract_areas() since original method is skipping this page - note that table needs to be selected
 area_table <- extract_areas(pdf_path_2024, pages = 12)
 
@@ -196,10 +210,9 @@ names(est_predation_clean)[5:6] <- c("CLR_Adult_LRS", "CLR_Adult_SNS_KLS")
 pred_raw_clean_2 <- pred_raw_clean_2 |>
   mutate(across(starts_with("UKL_Adult_") | starts_with("CLR_Adult_"), str_trim))
 
-# combine 2024
+# (3) combine 2024 with 2021-2023 to continue cleaning
 est_predation_clean <- bind_rows(pred_raw_clean, pred_raw_clean_2)
 
-# ========================================================= #
 
 # Row indices where the years are listed
 year_map <- tibble(row_index = c(4, 6, 8, 9, 10, 12, 13, 15),
@@ -261,6 +274,7 @@ group_by(location_1, fish_group_2, year) |>
          fish_group = fish_group_2) |>
   glimpse()
 
+# (4) clean and combined predation_estimates_wild
 predation_estimates_wild <- predation_estimates_wild_clean |>
   mutate(
   species = case_when(
@@ -277,35 +291,41 @@ predation_estimates_wild <- predation_estimates_wild_clean |>
   relocate(species, life_stage, origin, .after = location) |> glimpse()
 
 
+#### ======= estimates of predation rates PIT-tagged Sucker Assisted Rearing Program (SARP) juvenile suckers  ===== ####
 
- #### Estimates of predation rates PIT-tagged Sucker Assisted Rearing Program (SARP) juvenile suckers ----
+#  code below reads table 4 of 2021-2023 report, and table 4 of 2024 report
+
+# (1) read in 2021-2023 report table 3
  estimate_predation_sarp_raw <- tables[[4]] |> glimpse()
 
  estimate_predation_sarp_raw <- as.data.frame(estimate_predation_sarp_raw, stringsAsFactors = FALSE) |>
    clean_names()
 
- #TODO
-# -- read 2024 --
-# area_table_estimate_sarp <- extract_areas(pdf_path_2024, pages = 16)
-#
-# estimate_predation_sarp_raw_2024 <- as.data.frame(area_table_estimate_sarp, stringsAsFactors = FALSE) |>
-#    clean_names()
-
-
-
-
  estimate_predation_sarp <- estimate_predation_sarp_raw |>
    separate(col = 'upper_klamath_lake', # split the mixed column into two
-          into = c("ukl_sarp_fall_win", "ukl_chin_spr_sum"),
-          sep = "(?<=\\)|%)\\s+",
-          extra = "merge",
-          fill = "right") |>
+            into = c("ukl_sarp_fall_win", "ukl_chin_spr_sum"),
+            sep = "(?<=\\)|%)\\s+",
+            extra = "merge",
+            fill = "right") |>
    rename(ukl_chin_fall_win = x4,
           kr_chin_spr_sum = klamath_river,
           shpy_sarp_spr_sum = sheepy_lake,
           ukl_sarp_spr_sum = x2) |>
    glimpse()
 
+ #TODO
+# (2) read in 2024 report table 4
+# area_table_estimate_sarp <- extract_areas(pdf_path_2024, pages = 16)
+#
+# estimate_predation_sarp_raw_2024 <- as.data.frame(area_table_estimate_sarp, stringsAsFactors = FALSE) |>
+#    clean_names()
+
+ # TODO (3)
+ # 2024 looks slightly different than 2021-2023
+ # 2024 table needs to be cleaned and combined with 2021-2023
+
+
+# (4) functions to clean 2021-2023 table
  year_map_2 <- tibble(row_index = c(4, 6, 7, 9, 10, 12),
                     year = c(2021, 2021, 2022, 2022, 2023, 2023))
 
@@ -364,6 +384,8 @@ predation_estimates_wild <- predation_estimates_wild_clean |>
    rename(location = location_1,
           fish_group = fish_group_2)
 
+ # TODO
+ # (5) Combine 2024 with 2021-2023 table and produce clean and combined predation_estimates_hatchery
 predation_estimates_hatchery <- estimate_predation_sarp |>
   mutate(
    species = case_when(
@@ -385,6 +407,8 @@ predation_estimates_hatchery <- estimate_predation_sarp |>
    relocate(species, life_stage, origin, release_season, sarp_program, .after = location) |> glimpse()
 
 
+#### ======= harmonize and combine all tables  ===== ####
+#  predation_estimates_hatchery, predation_estimates_wild and predation_estimates_avian_pit_tag
 ## combine ----
 
 # harmonize hatchery
@@ -398,7 +422,7 @@ hatchery_long <- predation_estimates_hatchery |>
          percent_estimate, lower_ci_pct, upper_ci_pct) |> glimpse()
 
 # harmonize wild
-wild_long<- predation_estimates_wild |>
+wild_long<- predation_estimates_wild |> # this table is ready to be combined with others
   mutate(metric_type = "percent",
          release_season = NA_character_,
          # # origin = "Natural",
