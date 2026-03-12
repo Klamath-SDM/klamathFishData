ebda082b73265e3927d5006264eb348bd6e8b210
Badhia <byuneskatz@flowwest.com>
2026-03-12 11:51:19 -0700

    updates folder path on escapement script

--- data-raw/processing-scripts/process-escapement-megatable.R
+++ data-raw/processing-scripts/process-escapement-megatable.R
@@ -95,7 +95,7 @@ spring_spawner_escapement_clean <- filter(spring_megatable, section == "Spawning
 
 # Coho and steelhead are not included in the Megatable and are pulled from CDFW data table
 
-cdfw_population_raw <- read_xlsx(here::here("data-raw", "tables_with_data", "modeled", "Salmonid_Population_Monitoring_Data_CMPv2023.xlsx"), sheet = "Population Data")
+cdfw_population_raw <- read_xlsx(here::here("data-raw", "helper-data", "Salmonid_Population_Monitoring_Data_CMPv2023.xlsx"), sheet = "Population Data")
 
 klamath_cdfw_population_raw <- cdfw_population_raw |>
   filter(Watershed %in% c("Trinity River", "Scott River", "Shasta River", "Lower Klamath","Klamath River"))
